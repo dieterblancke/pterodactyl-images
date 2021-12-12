45b9574b050a161e3088604cf4fb73547de84d6d
Dieter Blancke <dieter.blancke@outlook.com>
2021-12-12 11:52:49 +0100

    satisfactory-experimental

--- satisfactory/entrypoint.sh
+++ satisfactory/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-steamcmd +login anonymous +force_install_dir /home/container/SatisfactoryDedicatedServer +app_update 1690800 +quit
+steamcmd +login anonymous +force_install_dir /home/container/SatisfactoryDedicatedServer +app_update 1690800 -beta public validate +quit
 
 cd /home/container/SatisfactoryDedicatedServer
 
